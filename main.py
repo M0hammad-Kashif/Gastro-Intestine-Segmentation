ba56eee028a080335484964429e4f87f789b2ccf
M0hammad-Kashif <iamkashif20@gmail.com>
2022-07-30 00:20:53 +0530

    main.py added

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
-from utils import *
-from imports import *
+import wandb
 
+from imports import *
+from model import build_model
+from optimizer import fetch_scheduler
+from train_val import run_training
+from utils import *
 
 set_seed(CFG.seed)
+
+model = build_model()
+optimizer = optim.Adam(model.parameters(), lr=CFG.lr, weight_decay=CFG.wd)
+scheduler = fetch_scheduler(optimizer)
+
+for fold in CFG.folds:
+    print(f'#' * 15)
+    print(f'### Fold: {fold}')
+    print(f'#' * 15)
+    run = wandb.init(project='uw-maddison-gi-tract',
+                     config={k: v for k, v in dict(vars(CFG)).items() if '__' not in k},
+                     anonymous=anonymous,
+                     name=f"fold-{fold}|dim-{CFG.img_size[0]}x{CFG.img_size[1]}|model-{CFG.model_name}",
+                     group=CFG.comment,
+                     )
+    train_loader, valid_loader = prepare_loaders(fold=fold, debug=CFG.debug)
+    model = build_model()
+    optimizer = optim.Adam(model.parameters(), lr=CFG.lr, weight_decay=CFG.wd)
+    scheduler = fetch_scheduler(optimizer)
+    model, history = run_training(model, optimizer, scheduler,
+                                  device=CFG.device,
+                                  num_epochs=CFG.epochs)
+    run.finish()
+    display(ipd.IFrame(run.url, width=1000, height=720))
